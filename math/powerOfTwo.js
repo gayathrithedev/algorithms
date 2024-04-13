f2f8374ddc5914bf4e7dd16fcf6f988cff0b0200
Gayathri Perumal <gayathriperumal100@gmail.com>
2024-04-13 21:55:18 +0530

    Power of two O(log(n))

---
+++ math/powerOfTwo.js
@@ -0,0 +1,10 @@
+// Power of Two
+// Below code Big-O is O(log(n))
+const isPowerOfTwo = (n) => {
+    if(n<=0) return false;
+   while(n>1) {
+    if(n%2 !== 0) return false;
+    n=n/2;
+   }
+   return true;
+ }
